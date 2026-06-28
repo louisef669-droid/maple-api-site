06ca43dbfbf82f4cb1f75463ae37d1f71021d978
louisef669 <louisef669@gmail.com>
2026-06-29 00:22:53 +0900

    Update preset dashboard

--- maple-api-site/lib/format.ts
+++
@@ -1,9 +0,0 @@
-export function formatNumber(value: any) {
-  const num = Number(String(value).replace(/,/g, ""));
-
-  if (isNaN(num)) {
-    return value;
-  }
-
-  return num.toLocaleString();
-}
